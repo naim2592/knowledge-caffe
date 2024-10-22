80034b635352fe5005c6b391ac713e71aff9fd8b
naim2592 <mddurjoyalok@gmail.com>
2024-10-22 11:44:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './component/header/Header'
+import './App.css'
+import Blogs from './component/blogs/Blogs'
+function App() {
+
+
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
